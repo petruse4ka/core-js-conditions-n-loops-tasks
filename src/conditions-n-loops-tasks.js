1bed63ec60a5a2b4575cad22a471aa0b8b5a2a44
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-11-17 00:56:43 +0100

    feat: add inefficient solution to task 14

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -489,8 +489,21 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let newString = str;
+  for (let i = 0; i < iterations; i += 1) {
+    let odd = '';
+    let even = '';
+    for (let j = 0; j < newString.length; j += 1) {
+      if (j % 2 === 0) {
+        even += newString[j];
+      } else {
+        odd += newString[j];
+      }
+    }
+    newString = even + odd;
+  }
+  return newString;
 }
 
 /**
